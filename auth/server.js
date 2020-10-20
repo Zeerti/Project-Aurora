e55972b348a4d9e8e41d245ba9a374c87e183c37
unknown <skarrj@gmail.com>
2020-10-20 14:26:55 -0600

    Version 0.5.1

--- auth/server.js
+++ auth/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -35,6 +36,16 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+// Serve static assets if in prod
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
