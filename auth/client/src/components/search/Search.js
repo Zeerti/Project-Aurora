48eb452c818c5c65b86879bb88e90cbadd2a9b27
unknown <skarrj@gmail.com>
2020-11-03 13:25:54 -0700

    First Pass on new search UI component

--- auth/client/src/components/search/Search.js
+++ auth/client/src/components/search/Search.js
@@ -26,7 +26,12 @@ let resultComponent;
 // TODO: Load CSV, Convert to JSON, Load into Redux State(searchData), Load from redux into local state(searchableData)
 function Search(props) {
   const [searchableData, setSearchableData] = useState(0);
-  const [userInput, setUserInput] = useState("");
+  const [formInputLocName, setFormInputLocName] = useState("");
+  const [formInputLID, setFormInputLID] = useState("");
+  const [formInputServer, setFormInputServer] = useState("");
+  const [formInputGroupName, setFormInputGroupName] = useState("");
+  const [formInputGID, setFormInputGID] = useState("");
+  const [formInputDBID, setFormInputDBID] = useState("");
   const [searchResults, setSearchResults] = useState(false);
   const [searchHistory, setSearchHistory] = useState([""]);
 
@@ -41,7 +46,7 @@ function Search(props) {
     e.preventDefault();
     props.addSearchHistory(e.target[0].value);
 
-    searchAllData(userInput, searchableData).then((matched) => {
+    searchAllData(formInputLocName, searchableData).then((matched) => {
       setSearchResults(matched);
       window.M.AutoInit();
     });
@@ -51,7 +56,7 @@ function Search(props) {
 
   const onClick = (e) => {
     setSearchResults(false);
-    searchAllData(userInput, searchableData).then((matched) => {
+    searchAllData(formInputLocName, searchableData).then((matched) => {
       setSearchResults(matched);
       window.M.AutoInit();
     });
@@ -72,8 +77,16 @@ function Search(props) {
 
   //   Update local state if redux currentInput changed
   useEffect(() => {
-    setUserInput(props.search.searchInput);
-  }, [props.search.currentInput, props.search.searchInput]);
+    setFormInputLocName(props.search.searchInput);
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('select');
+      var instances = window.M.FormSelect.init(elems);
+      console.log(instances);
+    })
+
+    
+
+  }, [props.search.currentInput]);
 
   // Update local state if redux searchResults changed
   useEffect(() => {
@@ -81,45 +94,162 @@ function Search(props) {
   }, [props.search.searchResults]);
 
   if (searchResults.length >= 1) {
+    // enableFullSearch allows more than 100 items to be rendered in the results component.
+    // Large results causes huge performance issues
     resultComponent = (
-      <Results results={searchResults} enableFullSearch={false} />
+      <Results results={searchResults} enableFullSearch={false} /> 
     );
   } else {
     resultComponent = false;
   }
   return (
+
+
     <div className="container">
-      <div className="col">
-        <div className="row">
-          {/* Search Field */}
-          <form noValidate onSubmit={onSubmit}>
-            <div className="input-field col s12">
-              <input
-                className="white-text"
-                value={userInput}
-                type="text"
-                id="username"
-                placeholder="What are we looking for today?"
-                onChange={(e) => {
-                  props.setCurrentInput(e.target.value);
-                  setUserInput(e.target.value);
-                }}
-              />
+      <form noValidate onSubmit={onSubmit}>
+        <div className="card blue-grey darken-1">
+          <div className="card-content">
+            <span className="card-title">Enter Search Details Below</span>
+            <div className="divider"></div>
+            <div className="row">
+              <div className="input-field col s12 l6 xl6">
+                <input
+                    className=""
+                    value={formInputLocName}
+                    type="text"
+                    id="formLocName"
+                    placeholder="Location Name"
+                    onChange={(e) => {
+                      props.setCurrentInput(e.target.value);
+                      setFormInputLocName(e.target.value);
+                    }}
+                  />
+                  <input
+                    className=""
+                    value={formInputLID}
+                    type="text"
+                    id="formLID"
+                    placeholder="Location ID"
+                    onChange={(e) => {
+                      setFormInputLID(e.target.value);
+                    }}
+                  />
+                  <input
+                    className=""
+                    value={formInputDBID}
+                    type="text"
+                    id="formDBID"
+                    placeholder="Database ID"
+                    onChange={(e) => {
+                      setFormInputDBID(e.target.value);
+                    }}
+                  />
+              </div>
+              <div className="input-field col s12 l6 xl6">
+                <select multiple>
+                  <option value="" disabled>Select Admin Portals</option>
+                  <option value ="all">All</option>
+                  <option value="1">Admin 1</option>
+                  <option value="2">Admin 2</option>
+                  <option value="3">Admin 3</option>
+                  <option value="1">Admin 4</option>
+                  <option value="2">Admin 5</option>
+                  <option value="3">Admin 6</option>
+                  <option value="1">Admin 7</option>
+                  <option value="2">Admin 8</option>
+                  <option value="3">Admin 9</option>
+                  <option value="1">Admin 10</option>
+                  <option value="2">Admin 11</option>
+                  <option value="3">Admin 12</option>
+                  <option value="1">Admin 13</option>
+                  <option value="2">Admin 14</option>
+                  <option value="3">Admin 15</option>
+                  <option value="1">Admin 16</option>
+                  <option value="2">Admin 17</option>
+                  <option value="3">Admin 18</option>
+                  <option value="1">Admin 19</option>
+                  <option value="2">Admin 20</option>
+                </select>
+                {/* <input
+                  className=""
+                  value={formInputServer}
+                  type="text"
+                  id="formServer"
+                  placeholder="Admin Portal Server"
+                  onChange={(e) => {
+                    setFormInputServer(e.target.value);
+                  }}
+                /> */}
+                <input
+                  className=""
+                  value={formInputGroupName}
+                  type="text"
+                  id="formGroupName"
+                  placeholder="Group Name"
+                  onChange={(e) => {
+                    setFormInputGroupName(e.target.value);
+                  }}
+                />
+                <input
+                  className=""
+                  value={formInputGID}
+                  type="text"
+                  id="formGID"
+                  placeholder="Group ID"
+                  onChange={(e) => {
+                    setFormInputGID(e.target.value);
+                  }}
+                />
+              </div>
             </div>
-          </form>
-          <button
-            type="submit"
-            className="btn btn-large waves-effect waves-light hoverable accent-3 bg-nord15"
-            onClick={onClick}
-          >
-            Search
-          </button>
+          </div>
         </div>
-      </div>
+      </form>
+      <button
+        type="submit"
+        className="btn btn-large waves-effect waves-light hoverable accent-3 bg-nord15 center"
+        onClick={onClick}
+      >
+        Search
+      </button>
 
       {/* Look up conditional rendering for React and don't render if search hasn't been done yet */}
       {resultComponent}
     </div>
+
+    // <div className="container">
+    //   <div className="col">
+    //     <div className="row">
+    //       {/* Search Field */}
+    //       <form noValidate onSubmit={onSubmit}>
+    //         <div className="input-field col s12">
+    //           <input
+    //             className="white-text"
+    //             value={formInputLocName}
+    //             type="text"
+    //             id="username"
+    //             placeholder="What are we looking for today?"
+    //             onChange={(e) => {
+    //               props.setCurrentInput(e.target.value);
+    //               setFormInputLocName(e.target.value);
+    //             }}
+    //           />
+    //         </div>
+    //       </form>
+    //     </div>
+    //   </div>
+
+    //   <button
+    //     type="submit"
+    //     className="btn btn-large waves-effect waves-light hoverable accent-3 bg-nord15 right"
+    //     onClick={onClick}
+    //   >
+    //     Search
+    //   </button>
+
+    //   {/* Look up conditional rendering for React and don't render if search hasn't been done yet */}
+    //   {resultComponent}
+    // </div>
   );
 }
 
@@ -134,10 +264,10 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addSearchHistory: (userInput) => dispatch(addSearchHistory(userInput)),
+    addSearchHistory: (formInputLocName) => dispatch(addSearchHistory(formInputLocName)),
     clearSearchHistory: () => dispatch(clearSearchHistory()),
-    setCurrentInput: (userInput) => dispatch(setCurrentInput(userInput)),
-    setSearchResults: (userInput) => dispatch(setSearchResults(userInput)),
+    setCurrentInput: (formInputLocName) => dispatch(setCurrentInput(formInputLocName)),
+    setSearchResults: (formInputLocName) => dispatch(setSearchResults(formInputLocName)),
     loadSearchableData: (data) => dispatch(loadSearchableData(data)),
   };
 };
